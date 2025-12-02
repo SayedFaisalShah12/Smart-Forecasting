a2af69addce950984f941407ebc999d94a50764d
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-12-02 23:18:10 +0500

    update

--- src/download_data.py
+++ src/download_data.py
@@ -9,16 +9,16 @@ def download_era5():
             'product_type': 'reanalysis',
             'variable': [
                 '2m_temperature', 'surface_pressure', 'total_precipitation',
-                '2m_dewpoint_temperature', 'u_component_of_wind_10m',
-                'v_component_of_wind_10m'
+                '2m_dewpoint_temperature', '10m_u_component_of_wind',
+                '10m_v_component_of_wind'
             ],
-            'year': '2025',
+            'year': '2023',
             'month': [f"{m:02d}" for m in range(1, 13)],
             'day': [f"{d:02d}" for d in range(1, 32)],
             'time': ['00:00', '06:00', '12:00', '18:00'],
             'format': 'netcdf'
         },
-        '../data/era5_2025.nc'
+        '../data/era5_2023.nc'
     )
 
 if __name__ == "__main__":
